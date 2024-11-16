07568e2998de3de7b7db2cbfa262ee836c19df47
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-16 20:41:56 +0530

    Update AuthorService.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/AuthorService.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/AuthorService.java
@@ -1,7 +1,12 @@
 package com.example.springsecurity.test.librarymanagementsystembackend.service;
 
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.AuthorDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.AuthorSaveDTO;
+
+import java.util.List;
 
 public interface AuthorService {
     String addAuthor(AuthorSaveDTO authorSaveDTO);
+
+    List<AuthorDTO> getAllAuthor();
 }
