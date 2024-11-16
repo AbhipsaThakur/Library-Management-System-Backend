cc6a4fb83c5bb185fd546a30d4b026f321990a4e
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-16 19:58:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library-Management-System-Backend
+ Using Spring Boot
---
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/LibraryManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.test.librarymanagementsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemBackendApplication.class, args);
+    }
+
+}
