5c9f06d0a2bb4facd00b536a3e2ded1d69ea03d2
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-16 20:05:23 +0530

    Create AuthorDTO.java

---
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/dto/AuthorDTO.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.test.librarymanagementsystembackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AuthorDTO {
+    private int authorid;
+
+}
