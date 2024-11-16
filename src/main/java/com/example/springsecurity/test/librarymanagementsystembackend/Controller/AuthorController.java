b57f87a8a0ded063fa23d83e41c8e103b57dee81
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-16 20:41:45 +0530

    Update AuthorController.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/Controller/AuthorController.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/Controller/AuthorController.java
@@ -1,9 +1,13 @@
 package com.example.springsecurity.test.librarymanagementsystembackend.Controller;
 
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.AuthorDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.AuthorSaveDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @CrossOrigin
 @RequestMapping("/api/v1/author")
@@ -16,5 +20,11 @@ public class AuthorController {
     public String saveAuthor(@RequestBody AuthorSaveDTO authorSaveDTO) {
         String authorname = authorService.addAuthor(authorSaveDTO);
         return "Added Successfully";
+    }
+
+    @GetMapping(path = "/getAllAuthor")
+    public List<AuthorDTO> getAllAuthor() {
+        List<AuthorDTO> allAuthors = authorService.getAllAuthor();
+        return allAuthors;
     }
 }
