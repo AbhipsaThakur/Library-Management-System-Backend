920a413b009aab74bda038d22f552c86baf6326d
8260994511 <164469438+8260994511@users.noreply.github.com>
2024-11-17 20:55:39 +0530

    Update BookService.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/BookService.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/BookService.java
@@ -1,7 +1,12 @@
 package com.example.springsecurity.test.librarymanagementsystembackend.service;
 
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.BookDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.BookSaveDTO;
+
+import java.util.List;
 
 public interface BookService {
     String addBook(BookSaveDTO bookSaveDTO);
+
+
 }
