24ca0b2dfcecf57f43fa6123777ba88658ff1fea
Rashmi-Ranjan-Prusty <130736738+Rashmi-Ranjan-Prusty@users.noreply.github.com>
2024-11-17 10:11:50 +0530

    Update PublisherService.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/PublisherService.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/PublisherService.java
@@ -1,8 +1,13 @@
 package com.example.springsecurity.test.librarymanagementsystembackend.service;
 
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.PublisherDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.PublisherSaveDTO;
 
+import java.util.List;
+
 public interface PublisherService {
-    
+
     String addPublisher(PublisherSaveDTO publisherSaveDTO);
+
+    List<PublisherDTO> getAllPublisher();
 }
