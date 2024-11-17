475aa49cf9cd7888c18992190308733a39d5406c
8260994511 <164469438+8260994511@users.noreply.github.com>
2024-11-17 18:31:28 +0530

    Create BookUpdateDTO.java

---
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/dto/BookUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.example.springsecurity.test.librarymanagementsystembackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BookUpdateDTO {
+    private int bookid;
+    private String booktitle;
+    private int authorid;
+    private int publisherId;
+}
