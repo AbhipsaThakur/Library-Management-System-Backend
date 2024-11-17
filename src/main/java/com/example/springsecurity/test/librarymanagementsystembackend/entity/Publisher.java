859b0f2386b67a12dbb0cad9f5b3cb2205a5777a
Rashmi-Ranjan-Prusty <130736738+Rashmi-Ranjan-Prusty@users.noreply.github.com>
2024-11-17 09:30:11 +0530

    Create Publisher.java

---
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/entity/Publisher.java
@@ -0,0 +1,53 @@
+package com.example.springsecurity.test.librarymanagementsystembackend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "publisherid", length = 11)
+    private int publisherId;
+
+    @Column(name = "publishername", length = 45)
+    private String publisherName;
+
+    public Publisher(int publisherId, String publisherName) {
+        this.publisherId = publisherId;
+        this.publisherName = publisherName;
+    }
+
+    public Publisher(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public Publisher() {
+
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "publisherId=" + publisherId +
+                ", publisherName='" + publisherName + '\'' +
+                '}';
+    }
+}
+
