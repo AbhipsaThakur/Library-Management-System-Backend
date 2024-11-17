d93f98de51afa01c4bf0e6e712e9311f7cd6194a
Rashmi-Ranjan-Prusty <130736738+Rashmi-Ranjan-Prusty@users.noreply.github.com>
2024-11-17 10:11:57 +0530

    Update PublisherServiceIMPL.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/IMPL/PublisherServiceIMPL.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/IMPL/PublisherServiceIMPL.java
@@ -1,11 +1,15 @@
 package com.example.springsecurity.test.librarymanagementsystembackend.service.IMPL;
 
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.PublisherDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.PublisherSaveDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.entity.Publisher;
 import com.example.springsecurity.test.librarymanagementsystembackend.repo.PublisherRepo;
 import com.example.springsecurity.test.librarymanagementsystembackend.service.PublisherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PublisherServiceIMPL implements  PublisherService {
@@ -18,5 +22,18 @@ public class PublisherServiceIMPL implements  PublisherService {
         Publisher publisher = new Publisher(publisherSaveDTO.getPublisherName());
         publisherRepo.save(publisher);
         return publisher.getPublisherName();
+    }
+
+    @Override
+    public List<PublisherDTO> getAllPublisher(){
+        List<Publisher> getpublishers = publisherRepo.findAll();
+        List<PublisherDTO> publisherDTOList = new ArrayList<>();
+        for (Publisher publisher : getpublishers) {
+            PublisherDTO publisherDTO = new PublisherDTO(
+                    publisher.getPublisherId(),
+                    publisher.getPublisherName()
+            );
+        }
+        return publisherDTOList;
     }
 }
