e9f25a56241557eac5cfcce17f78bd8b7264ce98
AbhipsaThakur <abhipsathakur933@gmail.com>
2024-11-25 09:26:52 +0530

    Update BookService.java

--- src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/BookService.java
+++ src/main/java/com/example/springsecurity/test/librarymanagementsystembackend/service/BookService.java
@@ -2,11 +2,17 @@ package com.example.springsecurity.test.librarymanagementsystembackend.service;
 
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.BookDTO;
 import com.example.springsecurity.test.librarymanagementsystembackend.dto.BookSaveDTO;
+import com.example.springsecurity.test.librarymanagementsystembackend.dto.BookUpdateDTO;
 
 import java.util.List;
 
 public interface BookService {
     String addBook(BookSaveDTO bookSaveDTO);
+    List<BookDTO> getAllBook();
 
+    BookDTO getBookByid(int id);
 
+    String updateBooks(BookUpdateDTO bookUpdateDTO);
+
+    String deleteBooks(int id);
 }
